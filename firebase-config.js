e17c4fb1991e4b9736906574feaf8bdf45f42459
lozanoTH <xzipser@gmail.com>
2026-01-31 14:03:47 -0500

    gran update

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDp6IB_ZndLgMWamLzThb5EBqClYSQf2LM",
   authDomain: "lozano-1690859356322.firebaseapp.com",
+  databaseURL: "https://lozano-1690859356322-default-rtdb.firebaseio.com",
   projectId: "lozano-1690859356322",
   storageBucket: "lozano-1690859356322.firebasestorage.app",
   messagingSenderId: "562516890895",
@@ -12,4 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export { signInWithEmailAndPassword };
+export const db = getDatabase(app);
+export { signInWithEmailAndPassword, ref, push };
