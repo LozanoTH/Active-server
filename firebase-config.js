3ad936c133ff1dc804d02137727c8c3757e1d65d
lozanoTH <xzipser@gmail.com>
2026-01-31 13:41:52 -0500

    login home update

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp6IB_ZndLgMWamLzThb5EBqClYSQf2LM",
+  authDomain: "lozano-1690859356322.firebaseapp.com",
+  projectId: "lozano-1690859356322",
+  storageBucket: "lozano-1690859356322.firebasestorage.app",
+  messagingSenderId: "562516890895",
+  appId: "1:562516890895:web:ba63485fe983cf037dda7a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export { signInWithEmailAndPassword };
